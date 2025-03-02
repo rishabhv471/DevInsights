20f4c5126c818e70971af6d75ad4d8d6ead918cd
Rishabh verma <25744589+rishabhv471@users.noreply.github.com>
2025-03-02 23:00:06 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ interface Environment {
 }               
 
 export const environment: Environment = {
-  production: false,
-  productHuntToken: 'Your Product Hunt Token',
-  deepseekApiKey: 'your deepseek api key',          
-  huggingfaceApiKey: 'your huggingface api key',
-  togetherApiKey: 'your together api key',  
-  replicateApiKey: 'your replicate api key',  
-}; 
+  production: false, 
+  productHuntToken: 'PvjxxLvcK8qKXtzdpCOLKgCUBOhybj-8sK3sItmhh_U', //expired
+  deepseekApiKey: 'sk-e18ae956595b4bc0b87067a9086aa2e3', //expired
+  huggingfaceApiKey: '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', //expired
+  togetherApiKey: 'qwer', //expired
+  replicateApiKey: 'r8_2ZxTxAAxaap0AkBOhB1UhkJQcyCdOWc2vM104' //expired
+};
